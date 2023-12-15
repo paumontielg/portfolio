ffae80082e0d1f3a2f31a45ed75610619d325f19
Paulina Montiel <laroux2345@gmail.com>
2023-12-15 16:03:26 -0600

    update main photo

--- README.md
+++ README.md
@@ -42,7 +42,3 @@ Además, se incluyeron **Google Fonts** para personalizar la fuente y **Bootstra
 ## Español
 
 El texto de la página web está escrito en español, al igual que las clases y atributos personalizados. Las clases relacionadas con Bootstrap se incluyeron en inglés.
-
-
-
-
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-let app = document.getElementById('typewriter');
- 
+let app = document.getElementById("typewriter");
+
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+  cursor: "<span style='color: #fb4993;'>|</span>",
 });
- 
+
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Enseño programación web y hago contenido de tecnología.</span>')
+  .typeString(
+    '<span style="color: #fb4993;">Enseño programación web y hago contenido de tecnología.</span>'
+  )
   .pauseFor(200)
   .deleteChars(10)
   .start();
